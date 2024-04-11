14103c3ee496cc1c272892cb10ae607feeda4d5a
Antonio <antw4ve@icloud.com>
2024-04-11 12:28:25 +0200

    basic views and models

---
+++ gamesquad/base/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from .models import Room
+# Create your views here.
+
+# rooms = [
+#     {'id': 1, 'name': 'Fortnite'},
+#     {'id': 2, 'name': 'DotA 2'},
+#     {'id': 3, 'name': 'Monster Hunter'},
+# ]
+
+def home(request):
+    rooms = Room.objects.all()
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
+
+def room(request, pk):
+    room = Room.objects.get(id=pk)
+    context = {'room' : room}
+    return render(request, 'base/room.html', context)
+
+def createRoom(request):
+    context = {}
+    return render(request, 'base/room_form.html', context)
