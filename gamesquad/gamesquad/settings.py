e50bc2d3be218311223163fe379ae1ce7637c5d8
Antonio <antw4ve@icloud.com>
2024-04-26 14:32:46 +0200

    extracted secrets to .env

--- gamesquad/gamesquad/settings.py
+++ gamesquad/gamesquad/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure--4^#m(9^@dv@feou1!hv_%@-wi_x7i8=l88jl(7fpy08%qztop"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.getenv("DEBUG")
 
 ALLOWED_HOSTS = [
     ".antwave.online",
