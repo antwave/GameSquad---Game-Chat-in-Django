1f3e8d47c5b29427c57d2931b5c109554cff0e91
Antonio <antw4ve@icloud.com>
2024-04-26 20:39:05 +0200

    added /gamesquad url location

--- gamesquad/base/urls.py
+++ gamesquad/base/urls.py
@@ -2,18 +2,17 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login/', views.loginView, name="login"),
-    path('logout/', views.logoutView, name="logout"),
-    path('register/', views.registerView, name="register"),
-    path('', views.home, name="home"),
-    path('room/<int:pk>/', views.room, name="room"),
-    path('profile/<int:pk>/', views.userProfile, name="user-profile"),
-    path('create-room/', views.createRoom, name="create-room"),
-    path('update-room/<int:pk>/', views.updateRoom, name="update-room"),
-    path('delete-room/<int:pk>/', views.deleteRoom, name="delete-room"),
-    path('delete-message/<int:pk>/', views.deleteMessage, name="delete-message"),
-    path('edit-user/', views.editUser, name="edit-user"),
-    path('games/', views.gamesPage, name="games"),
-    path('activities/', views.activityPage, name="activities"),
-
+    path("login/", views.loginView, name="login"),
+    path("logout/", views.logoutView, name="logout"),
+    path("register/", views.registerView, name="register"),
+    path("", views.home, name="home"),
+    path("room/<int:pk>/", views.room, name="room"),
+    path("profile/<int:pk>/", views.userProfile, name="user-profile"),
+    path("create-room/", views.createRoom, name="create-room"),
+    path("update-room/<int:pk>/", views.updateRoom, name="update-room"),
+    path("delete-room/<int:pk>/", views.deleteRoom, name="delete-room"),
+    path("delete-message/<int:pk>/", views.deleteMessage, name="delete-message"),
+    path("edit-user/", views.editUser, name="edit-user"),
+    path("games/", views.gamesPage, name="games"),
+    path("activities/", views.activityPage, name="activities"),
 ]
--- gamesquad/gamesquad/urls.py
+++ gamesquad/gamesquad/urls.py
@@ -21,9 +21,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", include("base.urls")),
-    path("api/", include("base.api.urls")),
+    path("gamesquad/admin/", admin.site.urls),
+    path("gamesquad/", include("base.urls")),
+    path("gamesquad/api/", include("base.api.urls")),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
