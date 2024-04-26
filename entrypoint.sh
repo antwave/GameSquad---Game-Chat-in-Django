dda807f02626a4bf70ea4125df8d161213528d41
Antonio <antw4ve@icloud.com>
2024-04-26 17:49:26 +0200

    fixed entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,8 @@
 #!bin/sh
 
-python gamesquad/manage.py migrate --no-input
-python gamesquad/manage.py collectstatic --no-input
+cd gamesquad
+
+python manage.py migrate --no-input
+python manage.py collectstatic --no-input
 
 daphne -p 8000 -b 0.0.0.0 gamesquad.asgi:application
